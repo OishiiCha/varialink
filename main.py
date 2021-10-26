174e60900281ffe7f1ab79e6e8c37c995a31de54
OishiiCha <86476845+OishiiCha@users.noreply.github.com>
2021-10-26 22:23:37 +0100

    initial

---
+++ main.py
@@ -0,0 +1,158 @@
+from tkinter import *
+import webbrowser
+from time import strftime
+from PIL import ImageTk, Image
+from datetime import date
+import calendar
+import csv
+
+# Data
+filename = "data/data.csv"
+lines = []
+
+with open(filename, 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    for line in csvreader:
+        line.append(line)
+
+if int(line[11]) == 1:
+    mysetting = 1
+    directory = "data/icons/light_mode/"
+    bgc = "lightsteelblue"
+    bc = "lightsteelblue3"
+    fc = "black"
+else:
+    mysetting = 0
+    directory = "data/icons/dark_mode/"
+    bgc = "grey12"
+    bc = "grey18"
+    fc = "white"
+
+
+class Win(Tk):
+
+    def __init__(self,master=None):
+        Tk.__init__(self,master)
+        self.overrideredirect(True)
+        self._offsetx = 0
+        self._offsety = 0
+        self.bind('<Button-1>',self.clickwin)
+        self.bind('<B1-Motion>',self.dragwin)
+
+    def dragwin(self,event):
+        x = self.winfo_pointerx() - self._offsetx
+        y = self.winfo_pointery() - self._offsety
+        self.geometry('+{x}+{y}'.format(x=x,y=y))
+
+    def clickwin(self,event):
+        self._offsetx = event.x
+        self._offsety = event.y
+
+
+win = Win()
+
+
+windowWidth = 600
+windowHeight = 500
+win.geometry(str(windowWidth)+"x"+str(windowHeight))
+positionRight = int(win.winfo_screenwidth()/2 - windowWidth/2)
+positionDown = int(win.winfo_screenheight()/2 - windowHeight/2)
+win.geometry("+{}+{}".format(positionRight, positionDown))
+win.configure(bg=bgc)
+win.resizable(width=False, height=False)
+
+xb = Button(win, text="x", width=3, bg=bgc, fg=fc, font=('arial', 25, 'bold'), borderwidth=0, highlightthickness=0, activebackground=bgc, command=win.destroy)
+xb.place(x=windowWidth, y=0, anchor="ne")
+
+
+if int(str(line[9])):
+    web_client = "https://pwa.zoom.us/wc/join/"
+else:
+    web_client = "https://pwa.zoom.us/j/"
+
+def jw_org():
+    jw_org_link = "https://jw.org"
+    webbrowser.open(jw_org_link, 1)
+
+def wol():
+    wol_link = "https://wol.jw.org"
+    webbrowser.open(wol_link, 1)
+
+def zoom_mw():
+    zoom_link = str(web_client+str(line[3]))
+    # https://pwa.zoom.us/wc/join/ --Meeting ID
+    webbrowser.open(zoom_link)
+
+def zoom_we():
+    zoom_link = str(web_client+str(line[5]))
+    # https://pwa.zoom.us/wc/join/ --Meeting ID
+    webbrowser.open(zoom_link)
+
+def notice():
+    notice_link = str(line[7])
+    webbrowser.open(notice_link)
+
+def meeting_choice():
+    if calendar.day_name[date.today().weekday()] in ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]:
+        zoom_mw()
+    else:
+        zoom_we()
+
+width_i = 320
+height_i = 100
+
+jw_photo = Image.open(str(directory+"jw_lrg.png"))
+jw_r = jw_photo.resize((width_i, height_i), Image.ANTIALIAS)
+jw_image = ImageTk.PhotoImage(jw_r)
+
+wol_photo = Image.open(str(directory+"wol_lrg.png"))
+wol_r = wol_photo.resize((width_i, height_i), Image.ANTIALIAS)
+wol_image = ImageTk.PhotoImage(wol_r)
+
+zoom_photo = Image.open(str(directory+"zoom_lrg.png"))
+zoom_r = zoom_photo.resize((width_i, height_i), Image.ANTIALIAS)
+zoom_image = ImageTk.PhotoImage(zoom_r)
+
+notice_photo = Image.open(str(directory+"notice_lrg.png"))
+notice_r = notice_photo.resize((width_i, height_i), Image.ANTIALIAS)
+notice_image = ImageTk.PhotoImage(notice_r)
+
+
+# Buttons
+b_width = 14
+button_frame = Frame(win, height=450, width=330, bg=bgc)
+button_frame.place(x=190, y=250, anchor="center")
+
+row1 = int((450/30)*5)
+row2 = int((450/30)*12)
+row3 = int((450/30)*19)
+row4 = int((450/30)*26)
+b_col = int(330/2)
+
+jw_org = Button(button_frame, image=jw_image, bg=bc, borderwidth=0, highlightthickness=0, activebackground=bgc, command=jw_org)
+jw_org.place(x=b_col, y=row1, anchor="center")
+
+wol = Button(button_frame, image=wol_image, bg=bc, borderwidth=0, highlightthickness=0, activebackground=bgc, command=wol)
+wol.place(x=b_col, y=row2, anchor="center")
+
+zoom = Button(button_frame, image=zoom_image, bg=bc, borderwidth=0, highlightthickness=0, activebackground=bgc, command=meeting_choice)
+zoom.place(x=b_col, y=row3, anchor="center")
+
+noticeboard = Button(button_frame, image=notice_image, bg=bc, borderwidth=0, highlightthickness=0, activebackground=bgc, command=notice)
+noticeboard.place(x=b_col, y=row4, anchor="center")
+
+
+# Time frame
+def timed():
+    string = strftime('%I:%M:%S %p\n\n%A\n%d %B\n%Y')
+    clock_text.config(text=string)
+    clock_text.after(1000, timed)
+
+tframe = Frame(win, height=300, width=200, bg=bgc)
+tframe.place(x=480, y=int(windowHeight/2), anchor="center")
+clock_text = Label(tframe, bg=bgc, fg=fc, font=('calibri', 25, 'bold'))
+clock_text.place(x=str(200/2), y=100, anchor="center")
+
+if __name__ == '__main__':
+    timed()
+    win.mainloop()
---
+++ settings.py
@@ -0,0 +1,309 @@
+from tkinter import *
+import csv
+import os.path
+import os
+import platform
+
+if platform.system() == "Windows":
+    cpc = 1
+elif platform.system() == "Linux":
+    cpc = 2
+elif platform.system() == "macOS":
+    cpc = 3
+else:
+    cpc = 4
+
+
+bgc = "light grey"
+
+setwindow = Tk()
+setwindow.geometry("700x300")
+setwindow.title("Settings")
+setwindow.configure(background=bgc)
+setwindow.resizable(width=False, height=False)
+if cpc == 1:
+    setwindow.iconbitmap('data/links.ico')
+
+# rows
+row1 = 25
+row2 = 50
+row3 = 75
+row4 = 100
+row5 = 125
+row6 = 160
+row7 = 195
+row8 = 255
+
+# columns
+column_l = 75
+column_s = 275
+column_c = 550
+
+frame1 = LabelFrame(setwindow,
+                    text="Set Configuration",
+                    font=('arial', 10, 'bold'),
+                    height=275,
+                    width=250,
+                    bg=bgc)
+frame1.place(x=column_s,
+             y=150,
+             anchor="center")
+
+frame2 = LabelFrame(setwindow,
+                    text="Current Configuration",
+                    font=('arial', 10, 'bold'),
+                    height=275,
+                    width=250,
+                    bg=bgc)
+frame2.place(x=column_c,
+             y=150,
+             anchor="center")
+
+
+# font
+l_font = ('arial', 10, 'bold')
+t_font = ('arial', 15, 'bold')
+
+b_width = 30
+
+file = 'data/data.csv'
+
+
+def read():
+    with open(file, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        for line in csvreader:
+            line.append(line)
+
+    cong = str(line[1])
+    mw_id = str(line[3])
+    we_id = str(line[5])
+    nbl = str(line[7])
+    if int(line[9]) == 1:
+        client = "Web Client"
+    else:
+        client = "Zoom App"
+    if int(line[11]) == 1:
+        mode = "Light Mode"
+    else:
+        mode = "Dark Mode"
+
+    c_cong = Label(setwindow,
+                   text=cong,
+                   bg=bgc,
+                   fg="black",
+                   justify="center",
+                   font=l_font,
+                   width=30)
+    c_cong.place(x=column_c,
+                 y=row2,
+                 anchor="center")
+
+    c_mw_id = Label(setwindow,
+                    text=mw_id,
+                    bg=bgc,
+                    fg="black",
+                    justify="center",
+                    font=l_font,
+                    width=30)
+    c_mw_id.place(x=column_c,
+                  y=row3,
+                  anchor="center")
+
+    c_we_id = Label(setwindow,
+                    text=we_id,
+                    bg=bgc,
+                    fg="black",
+                    justify="center",
+                    font=l_font,
+                    width=30)
+    c_we_id.place(x=column_c,
+                  y=row4,
+                  anchor="center")
+
+    c_nbl = Label(setwindow,
+                  text=nbl,
+                  bg=bgc,
+                  fg="black",
+                  justify="center",
+                  font=l_font,
+                  width=30)
+    c_nbl.place(x=column_c,
+                y=row5,
+                anchor="center")
+
+    c_client = Label(setwindow,
+                     text=client,
+                     bg=bgc,
+                     fg="black",
+                     justify="center",
+                     font=l_font,
+                     width=30)
+    c_client.place(x=column_c,
+                   y=row6,
+                   anchor="center")
+
+    c_cm = Label(setwindow,
+                 text=mode,
+                 bg=bgc,
+                 fg="black",
+                 justify="center",
+                 font=l_font,
+                 width=30)
+    c_cm.place(x=column_c,
+               y=row7,
+               anchor="center")
+
+def setconfig():
+    if os.path.isfile(file):
+        os.remove(file)
+    f = open(file, "x", newline="")
+    cong_g = str(cong_ent.get())
+    zm_g = str(zm_ent.get())
+    zw_g = str(zw_ent.get())
+    nbl_g = str(nb_ent.get())
+    if variable.get() == "Web Client":
+        c_g = 1
+    else:
+        c_g = 0
+    if variable2.get() == "Light Mode":
+        m_g = 1
+    else:
+        m_g = 0
+
+    new_data_string = ("congregation",
+                       cong_g,
+                       "zoom id (midweek)",
+                       zm_g,"zoom id (weekend)",
+                       zw_g,"noticeboard",
+                       nbl_g,
+                       "client",
+                       c_g,
+                       "colour",
+                       m_g)
+    writer = csv.writer(f)
+    writer.writerow(new_data_string)
+    f.close()
+    read()
+
+
+cong_l = Label(setwindow,
+               text="Congregation Name",
+               bg=bgc,
+               fg="black",
+               font=l_font)
+cong_l.place(x=column_l,
+             y=row2,
+             anchor="center")
+cong_ent = Entry(setwindow,
+                 justify="center",
+                 width=b_width,
+                 font=l_font)
+cong_ent.place(x=column_s,
+               y=row2,
+               anchor="center")
+
+zml = Label(setwindow,
+            text="Zoom Midweek ID",
+            bg=bgc,
+            fg="black",
+            justify="center",
+            font=l_font)
+zml.place(x=column_l,
+          y=row3,
+          anchor="center")
+zm_ent = Entry(setwindow,
+               justify="center",
+               width=b_width,
+               font=l_font)
+zm_ent.place(x=column_s,
+             y=row3,
+             anchor="center")
+
+zwl = Label(setwindow,
+            text="Zoom Weekend ID",
+            bg=bgc,
+            fg="black",
+            justify="center",
+            font=l_font)
+zwl.place(x=column_l,
+          y=row4,
+          anchor="center")
+zw_ent = Entry(setwindow,
+               justify="center",
+               width=b_width,
+               font=l_font)
+zw_ent.place(x=column_s,
+             y=row4,
+             anchor="center")
+
+nbt = Label(setwindow,
+            text="Noticeboard Link",
+            bg=bgc,
+            fg="black",
+            justify="center",
+            font=l_font)
+nbt.place(x=column_l,
+          y=row5,
+          anchor="center")
+nb_ent = Entry(setwindow,
+               justify="center",
+               width=b_width,
+               font=l_font)
+nb_ent.place(x=column_s,
+             y=row5,
+             anchor="center")
+
+client_l = Label(setwindow,
+                 text="Client",
+                 bg=bgc,
+                 fg="black",
+                 justify="center",
+                 font=l_font)
+client_l.place(x=column_l,
+               y=row6,
+               anchor="center")
+options = ["Web Client","Zoom App"]
+variable = StringVar(setwindow)
+variable.set(options[0])
+client_ent = OptionMenu(setwindow,
+                        variable,
+                        *options)
+client_ent.place(x=column_s,
+                 y=row6,
+                 anchor="center")
+
+mode_l = Label(setwindow,
+                 text="Colour Mode",
+                 bg=bgc,
+                 fg="black",
+                 justify="center",
+                 font=l_font)
+mode_l.place(x=column_l,
+               y=row7,
+               anchor="center")
+
+options2 = ["Light Mode","Dark Mode"]
+variable2 = StringVar(setwindow)
+variable2.set(options2[0])
+mode_ent = OptionMenu(setwindow,
+                        variable2,
+                        *options2)
+mode_ent.place(x=column_s,
+                 y=row7,
+                 anchor="center")
+
+
+
+entbutton = Button(setwindow,
+                   text="Set new data",
+                   justify="center",
+                   font=('arial', 10, 'bold'),
+                   command=setconfig)
+entbutton.place(x=column_s,
+                y=row8,
+                anchor="center")
+
+if __name__ == '__main__':
+    read()
+    setwindow.mainloop()
